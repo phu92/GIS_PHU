864a2d107e0608db3774856e13a36a58114e1639
Park Hyun-uk <teroto@naver.com>
2021-07-13 12:35:50 +0900

    7.13 django commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # # Create your views here.
 # def hello_world(request):
 #     return render(request, 'accountapp/hello_world.html')
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import NewModel
 
@@ -23,3 +26,12 @@ def hello_world(request):
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list': data_list})
+
+
+
+class AccountCreateView(CreateView):
+    model =  User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    #reverse 그대로 사용시 에러 발생, 함수와 클래스의 불러오는 방식이 다르기 때문
+    template_name = 'accountapp/create.html'
