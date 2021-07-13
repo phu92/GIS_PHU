5c7f9998547d8b57b6778d4b6e89f5a982ed9273
Park Hyun-uk <teroto@naver.com>
2021-07-13 11:35:26 +0900

    7.13 django commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
 
 # # Create your views here.
 # def hello_world(request):
 #     return render(request, 'accountapp/hello_world.html')
+from django.urls import reverse
+
 from accountapp.models import NewModel
 
 
@@ -13,13 +14,12 @@ def hello_world(request):
 
         temp = request.POST.get('input_text')
 
-        new_model =  NewModel()
-        new_model.text = temp# temp의 값을 객체 변수명에 저장
-        new_model.save()#DB에저장
-        data_list = NewModel.objects.all()
-
-        return render(request, 'accountapp/hello_world.html',
-                      context={'data_list':data_list})
+        new_model = NewModel()
+        new_model.text = temp  # temp의 값을 객체 변수명에 저장
+        new_model.save()  # DB에저장
+        # 절대 주소 대신 라우터 재연결 하게
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
+        data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'data_list':data_list})
+                      context={'data_list': data_list})
