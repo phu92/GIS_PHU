9f6480dd8c5f20a3d8c33404564ea2912caa2164
Park Hyun-uk <teroto@naver.com>
2021-07-20 11:25:15 +0900

    7.20 django commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 # def hello_world(request):
 #     return render(request, 'accountapp/hello_world.html')
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import NewModel
 
@@ -40,3 +40,11 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView): #crete view 참조
+    model = User
+    form_class = UserCreationForm
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world')
+    #success_url = reverse_lazy('accountapp:detail') #아직 사용 불가
+    template_name = 'accountapp/update.html'
