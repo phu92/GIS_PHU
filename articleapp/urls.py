fe980b81d3fb093e8723d9fc5834aef58eb4aff3
Park Hyun-uk <teroto@naver.com>
2021-07-30 12:30:23 +0900

    7.30 django commit 4

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,4 +1,9 @@
 from django.conf.urls.i18n import urlpatterns
+from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = 'articleapp'
 
 urlpatterns = [
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list')
 ]
