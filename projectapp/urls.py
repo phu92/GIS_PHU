78c896caf49b0b36abf388b7fe9483ebf11f65c0
Park Hyun-uk <teroto@naver.com>
2021-08-17 12:47:18 +0900

    8.17 django commit 2,3

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from projectapp.views import ProjectCreateView, ProjectDetailView
+from projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListView
 
 app_name = 'projectapp'
 
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
-
+    path('list/', ProjectListView.as_view(), name='list'),
 
 ]
