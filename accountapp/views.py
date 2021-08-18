73d54bed9388adb8cb6b2ec94f5d66f68a01bc6e
Park Hyun-uk <teroto@naver.com>
2021-08-18 18:06:36 +0900

    8.18 django commit 4

--- accountapp/views.py
+++ accountapp/views.py
@@ -10,10 +10,12 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 from accountapp.templates.accountapp.decorators import account_ownership_required
+from articleapp.models import Article
 
 
 @login_required
@@ -40,10 +42,18 @@ class AccountCreateView(CreateView):
     #reverse 그대로 사용시 에러 발생, 함수와 클래스의 불러오는 방식이 다르기 때문
     template_name = 'accountapp/create.html'
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(writer=self.object)
+        return super().get_context_data(object_list=article_list,
+                                        **kwargs)
+
 
 has_ownership = [login_required, account_ownership_required]
 
