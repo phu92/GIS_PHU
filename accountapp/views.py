83e54a3e4a37e2e3be9e6ec9353e6415b32a8384
Park Hyun-uk <teroto@naver.com>
2021-08-27 11:16:11 +0900

    8.27 django commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,8 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidde
 from django.shortcuts import render
 
 # # Create your views here.
-# def hello_world(request):
-#     return render(request, 'accountapp/hello_world.html')
+
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
@@ -17,28 +16,11 @@ from accountapp.models import NewModel
 from accountapp.templates.accountapp.decorators import account_ownership_required
 from articleapp.models import Article
 
-
-@login_required
-def hello_world(request):
-        if request.method == "POST":
-
-            temp = request.POST.get('input_text')
-
-            new_model = NewModel()
-            new_model.text = temp  # temp의 값을 객체 변수명에 저장
-            new_model.save()  # DB에저장
-            # 절대 주소 대신 라우터 재연결 하게
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            data_list = NewModel.objects.all()
-            return render(request, 'accountapp/hello_world.html',
-                          context={'data_list': data_list})
-
 #class based view
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     #reverse 그대로 사용시 에러 발생, 함수와 클래스의 불러오는 방식이 다르기 때문
     template_name = 'accountapp/create.html'
 
@@ -74,5 +56,5 @@ class AccountUpdateView(UpdateView): #crete view 참조
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'#유저 정보에 접근하기 위해
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
